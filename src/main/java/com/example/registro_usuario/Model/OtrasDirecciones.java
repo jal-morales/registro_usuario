fa0c2f9065d77f3ab713c51a2ec6b69ff5410754
jal-morales <jal.morales89@gmail.com>
2024-04-12 10:52:04 -0400

    Se  Trabaja en modelo otras direcciones donde se agrega anotaciones para relacionar tabla de registro de usuario con otras direcciones

--- src/main/java/com/example/registro_usuario/Model/OtrasDirecciones.java
+++ src/main/java/com/example/registro_usuario/Model/OtrasDirecciones.java
@@ -1,5 +1,68 @@
 package com.example.registro_usuario.Model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+@Entity
+@Table(name = "otras_direcciones")
 public class OtrasDirecciones {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "id_od")
+    private int id_od;
+
+    @Column(name = "direccion_od")
+    private String direccion_od;
+
+    @Column(name = "comuna_od")
+    private String comuna_od;
+
+    @Column(name = "ciudad_od")
+    private String ciudad_od;
+
+    @ManyToOne
+    @JoinColumn(name = "id_ru")
+    private RegistroUsuario registroUsuario;
+
+    public int getId_od() {
+        return this.id_od;
+    }
+
+    public void setId_od(int id_od) {
+        this.id_od = id_od;
+    }
+    public String getDireccion_od() {
+        return this.direccion_od;
+    }
+
+    public void setDireccion_od(String direccion_od) {
+        this.direccion_od = direccion_od;
+    }
+    public String getComuna_od() {
+        return this.comuna_od;
+    }
+
+    public void setComuna_od(String comuna_od) {
+        this.comuna_od = comuna_od;
+    }
+    public String getCiudad_od() {
+        return this.ciudad_od;
+    }
+
+    public void setCiudad_od(String ciudad_od) {
+        this.ciudad_od = ciudad_od;
+    }
+    public RegistroUsuario getRegistroUsuario() {
+        return this.registroUsuario;
+    }
+
+    public void setRegistroUsuario(RegistroUsuario registroUsuario) {
+        this.registroUsuario = registroUsuario;
+    }
 }
